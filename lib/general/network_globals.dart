222b49a6854c49aa65f63673f29397997edb84b6
lepups <nicolascarraro104@gmail.com>
2024-08-22 21:41:20 +0200

    baseurlk

--- lib/general/network_globals.dart
+++ lib/general/network_globals.dart
@@ -2,6 +2,7 @@ library globals;
 
 // String baseURL = "http://localhost:3000";
 // String baseURL = "http://85.208.51.194:3000";
-String baseURL = "https://digipaw.duckdns.org";
+// String baseURL = "https://digipaw.duckdns.org";
+String baseURL = "https://api.tailfur-find.com";
 // String baseURL = "https://localhost:3000";
 String s3BaseUrl = "https://ams1.vultrobjects.com/";
